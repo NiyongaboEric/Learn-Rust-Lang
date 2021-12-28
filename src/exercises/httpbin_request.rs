d71c5feaae85c34c6d27de7188e0942acc005aed
ENiyongabo <niyongaboeric00@gmail.com>
2021-12-28 10:05:36 +0200

    other way to send a request

--- src/exercises/httpbin_request.rs
+++ src/exercises/httpbin_request.rs
@@ -1,18 +1,19 @@
 use reqwest;
-use std::collections::HashMap;
 use std::error::Error;
+use std::collections::HashMap;
+
 
 #[tokio::main]
-pub async fn ipaddres() -> Result<i32, Box<dyn Error>> {
-	let response = reqwest::get("https://httpbin.org/ip")
-			.await?
-			.json::<HashMap<String, String>>()
-			.await?;
-	// println!("My PC IP Addres is {:#?}", response);
-	// Ok(());
-	// (e) => println!('e')
-	// "My PC IP Addres is {:#?}", response
-	Ok((c_string) => Ok("My PC IP Addres is {:#?}", response))
-
-	Err("Error IP address is not fetched".into());
+pub async fn ipaddres() -> Result<(), Box<dyn Error>> {
+	let url: &str = "https://httpbin.org/ip";
+	let client = reqwest::Client::new();
+	let res = client
+		.get(url)
+		.header("Accept", "text/plain")
+		.send()
+		.await?
+		.json::<HashMap<String, String>>()
+		.await?;
+	println!("My PC IP Addres is {:#?}", res);
+	Ok(())
 }
