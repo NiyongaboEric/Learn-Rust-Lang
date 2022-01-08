83b8170f400721301fb2b7da5d0a37b0ea14995b
ENiyongabo <niyongaboeric00@gmail.com>
2022-01-08 16:19:09 +0200

    add test

--- src/exercises/httpbin_request.rs
+++ src/exercises/httpbin_request.rs
@@ -15,3 +15,22 @@ pub async fn ipaddres() -> Result<String, Box<dyn Error>> {
 		.await?;
 	Ok(res)
 }
+
+
+#[cfg(test)]
+mod tests {
+
+	#[tokio::test]
+	async fn ipaddres() {
+		let url: &str = "https://httpbin.org/ip";
+		let client =reqwest::Client::new();
+
+		let response = client
+			.get(url)
+			.send()
+			.await
+			.expect("0000.0000.0000");
+		
+		assert!(response.status().is_success());
+	}
+}
