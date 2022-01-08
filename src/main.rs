b135f73ed74576a66857d1f2c876b667ceaf9cc2
Niyongabo <33821734+NiyongaboEric@users.noreply.github.com>
2022-01-08 16:28:02 +0200

    Revert "Send a get request to HTTPBin"

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-#[path="./exercises/httpbin_request.rs"]
-mod httpbin_request;
-
+mod get_request
 
 fn main() {
-	match httpbin_request::ipaddres() {
-		Ok(result) => println!("Yeah! Your Ip address is: {}", result),
-		Err(e) => eprintln!("Oop!, we can't find your ip address :( \n  {}", e),
-	}
+    get_request::run()
 }
